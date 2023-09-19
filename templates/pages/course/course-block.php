99758d69933a6c09212dd556a4d57a8f2a8e880d
Safari1367 <maryamsafarim82@gmail.com>
2023-09-20 01:05:05 +0330

    write

---
+++ templates/pages/course/course-block.php
@@ -0,0 +1,11 @@
+
+<div class="course-block">
+    <div class="course-info">
+        <div class="title"></div>
+        <div class="description"></div>
+        <div class="detail"></div>
+        <div class="btn"></div>
+    </div>
+    <div class="course-img"></div>
+    <div class="course-form"></div>
+</div>
