0fd90cbb4fdc8a66c0a3ea8cde3acd57c65a3003
Safari1367 <maryamsafarim82@gmail.com>
2023-09-20 07:48:01 +0330

    write responsive front page

--- templates/pages/front/start.php
+++ templates/pages/front/start.php
@@ -18,8 +18,18 @@ $startSliders = get_field('slider_start_section', $frontId);
                             <div class="start-img">
                                 <div class="shape <?= $Slide['start_slider_shape'] ?>"></div>
                                 <?= wp_get_attachment_image($Slide['start_slider_image'], 'full', false, []); ?>
-                                <div class="start-img-shadow"></div>
-                                <div class="video"><?= $Slide['start_slider_video'] ?></div>
+
+
+                                <div class="video-content" data-id=" ">
+                                    <img src="<?= get_stylesheet_directory_uri() ?>/imgs/video.png" alt="video">
+
+                                    <i class="icon-play play-video" id="play-video"></i>
+                                    <div class="video-popup">
+                                        <video id="mainVideo" controls>
+                                            <source src="<?= $Slide['start_slider_video'] ?>" type="video/mp4">
+                                        </video>
+                                    </div>
+                                </div>
                             </div>
                             <div class="start-text">
                                 <div class="start-text-title">
@@ -27,14 +37,22 @@ $startSliders = get_field('slider_start_section', $frontId);
                                 </div>
                                 <div class="panels-navigation">
                                     <?php if ($num == 1) : ?>
-                                        <div class="nav-panel next" data-sign="plus"><a href="#start-<?= $num + 1 ?>" class="anchor"><i class="icon-arrow-single-big"></i>next</a>
+                                        <div class="nav-panel next" data-sign="plus"><a href="#start-<?= $num + 1 ?>"
+                                                                                        class="anchor"><i
+                                                        class="icon-arrow-single-big"></i>next</a>
                                         </div>
                                     <?php elseif ($num == count($startSliders)) : ?>
-                                        <div class="nav-panel prv" data-sign="minus"><a href="#start-<?= $num - 1 ?>" class="anchor"><i class="icon-arrow-single-big"></i>prv</a></div>
+                                        <div class="nav-panel prv" data-sign="minus"><a href="#start-<?= $num - 1 ?>"
+                                                                                        class="anchor"><i
+                                                        class="icon-arrow-single-big"></i>prv</a></div>
                                     <?php else : ?>
-                                        <div class="nav-panel next" data-sign="plus"><a href="#start-<?= $num + 1 ?>" class="anchor"><i class="icon-arrow-single-big"></i>next</a>
+                                        <div class="nav-panel next" data-sign="plus"><a href="#start-<?= $num + 1 ?>"
+                                                                                        class="anchor"><i
+                                                        class="icon-arrow-single-big"></i>next</a>
                                         </div>
-                                        <div class="nav-panel prv" data-sign="minus"><a href="#start-<?= $num - 1 ?>" class="anchor"><i class="icon-arrow-single-big"></i>prv</a></div>
+                                        <div class="nav-panel prv" data-sign="minus"><a href="#start-<?= $num - 1 ?>"
+                                                                                        class="anchor"><i
+                                                        class="icon-arrow-single-big"></i>prv</a></div>
 
                                     <?php endif; ?>
                                 </div>
@@ -47,13 +65,14 @@ $startSliders = get_field('slider_start_section', $frontId);
                         </div>
 
                         <div class="breadcrumb-btn">
-                            <a href="#start-<?= ($num == count($startSliders)) ? $num - 1 : $num + 1 ?>" class="btn-right"><i class="icon-arrow-side-right"></i>مربی ها و هنرجوها</a>
+                            <a href="#start-<?= ($num == count($startSliders)) ? $num - 1 : $num + 1 ?>"
+                               class="btn-right"><i class="icon-arrow-side-right"></i>مربی ها و هنرجوها</a>
                             <a href="#panel-3" class="btn-left">اسلایدر<i class="icon-arrow-side-left"></i></a>
                         </div>
                     </div>
                 </div>
             </article>
-        <?php $num++;
+            <?php $num++;
         endforeach; ?>
     </div>
 </section>
